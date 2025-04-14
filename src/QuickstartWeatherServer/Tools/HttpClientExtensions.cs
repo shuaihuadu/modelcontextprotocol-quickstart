ac56e527e9de713ccfc0836ec883a3583bf32fe6
ShuaiHua Du <shuaihua.du@outlook.com>
2025-04-14 14:52:30 +0800

    Add examples.

---
+++ src/QuickstartWeatherServer/Tools/HttpClientExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace QuickstartWeatherServer;
+
+internal static class HttpClientExtensions
+{
+    public static async Task<JsonDocument> ReadJsonDocumentAsync(this HttpClient client, string requestUri)
+    {
+        using var response = await client.GetAsync(requestUri);
+
+        response.EnsureSuccessStatusCode();
+
+        return await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+    }
+}
