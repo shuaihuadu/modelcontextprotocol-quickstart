630b0032263cb34b194d4718fa82bbc50b1ea920
ShuaiHua Du <shuaihua.du@outlook.com>
2025-04-16 00:51:16 +0800

    Add examples.

---
+++ src/AspNetCoreSseServer/Program.cs
@@ -0,0 +1,12 @@
+using AspNetCoreSseServer.Tools;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMcpServer()
+    .WithTools<EchoTool>()
+    .WithTools<SampleLlmTool>();
+
+var app = builder.Build();
+
+app.MapMcp();
+app.Run();
---
+++ src/AspNetCoreSseServer/Tools/EchoTool.cs
@@ -0,0 +1,14 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace AspNetCoreSseServer.Tools;
+
+[McpServerToolType]
+public sealed class EchoTool
+{
+    [McpServerTool, Description("Echoes the input back to the client.")]
+    public static string Echo(string message)
+    {
+        return $"Hello {message}";
+    }
+}
